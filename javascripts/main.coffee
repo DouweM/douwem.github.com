79e72bfa9842c7e6b2dbb22f909ef83954f6f549
Douwe Maan <douwemaan@me.com>
2012-04-02 21:47:28 +0200

    Allow for navigation by arrow keys.

--- javascripts/main.coffee
+++ javascripts/main.coffee
@@ -3,9 +3,11 @@ $(document).ready ->
   $(window).on "resize", setLastSectionMinHeight
   
   $("header nav a").on "click", handleNavLinkClick
+  
+  $(window).on "keydown", handleKeyDown
 
 verticallyCenterTextEl = (el) ->    
-  elCenter = el.offset().top + (el.height() / 2)
+  elCenter = el.offset().top + (el.outerHeight() / 2)
   windowCenter = $(window).height() / 2
   
   $(window).scrollTop Math.ceil(elCenter - windowCenter)
@@ -13,7 +15,7 @@ verticallyCenterTextEl = (el) ->
 setLastSectionMinHeight = ->
   lastSection = $("#traits section").last()
   headerEl    = $("h2", lastSection)
-  lastSectionMinHeight = ($(window).height() / 2) + (headerEl.height() / 2)
+  lastSectionMinHeight = ($(window).height() / 2) + (headerEl.outerHeight() / 2)
   lastSection.css "min-height", Math.ceil(lastSectionMinHeight)
 
 handleNavLinkClick = (event) ->  
@@ -26,3 +28,32 @@ handleNavLinkClick = (event) ->
   
   verticallyCenterTextEl headerEl
 
+handleKeyDown = (event) ->
+  return unless event.which in [38, 40] # Up, Down
+  event.preventDefault()
+  
+  scrollCenter = $(window).scrollTop() + ($(window).height() / 2)
+  
+  centeredEl = null
+  centerableEls = $("section h2, section ul li")
+  lastElBottom = 0
+  for el in centerableEls
+    el = $(el)
+    offset = el.offset()
+    elBottom = (offset.top + el.outerHeight())
+    
+    if lastElBottom <= scrollCenter <= elBottom
+      centeredEl = el
+      break
+      
+    lastElBottom = elBottom
+  
+  centeredEl = centerableEls.last() if centeredEl == null
+  
+  centeredElIndex = centerableEls.index centeredEl
+  
+  otherIndex = if event.which == 38 then centeredElIndex - 1 else centeredElIndex + 1
+  return unless 0 <= otherIndex < centerableEls.length
+  
+  otherEl = centerableEls.eq(otherIndex)
+  verticallyCenterTextEl otherEl
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,16 @@
 (function() {
-  var handleNavLinkClick, setLastSectionMinHeight, verticallyCenterTextEl;
+  var handleKeyDown, handleNavLinkClick, setLastSectionMinHeight, verticallyCenterTextEl;
 
   $(document).ready(function() {
     setLastSectionMinHeight();
     $(window).on("resize", setLastSectionMinHeight);
-    return $("header nav a").on("click", handleNavLinkClick);
+    $("header nav a").on("click", handleNavLinkClick);
+    return $(window).on("keydown", handleKeyDown);
   });
 
   verticallyCenterTextEl = function(el) {
     var elCenter, windowCenter;
-    elCenter = el.offset().top + (el.height() / 2);
+    elCenter = el.offset().top + (el.outerHeight() / 2);
     windowCenter = $(window).height() / 2;
     return $(window).scrollTop(Math.ceil(elCenter - windowCenter));
   };
@@ -18,7 +19,7 @@
     var headerEl, lastSection, lastSectionMinHeight;
     lastSection = $("#traits section").last();
     headerEl = $("h2", lastSection);
-    lastSectionMinHeight = ($(window).height() / 2) + (headerEl.height() / 2);
+    lastSectionMinHeight = ($(window).height() / 2) + (headerEl.outerHeight() / 2);
     return lastSection.css("min-height", Math.ceil(lastSectionMinHeight));
   };
 
@@ -30,6 +31,33 @@
     sectionEl = $(href);
     headerEl = $("h2", sectionEl);
     return verticallyCenterTextEl(headerEl);
+  };
+
+  handleKeyDown = function(event) {
+    var centerableEls, centeredEl, centeredElIndex, el, elBottom, lastElBottom, offset, otherEl, otherIndex, scrollCenter, _i, _len, _ref;
+    if ((_ref = event.which) !== 38 && _ref !== 40) return;
+    event.preventDefault();
+    scrollCenter = $(window).scrollTop() + ($(window).height() / 2);
+    centeredEl = null;
+    centerableEls = $("section h2, section ul li");
+    lastElBottom = 0;
+    for (_i = 0, _len = centerableEls.length; _i < _len; _i++) {
+      el = centerableEls[_i];
+      el = $(el);
+      offset = el.offset();
+      elBottom = offset.top + el.outerHeight();
+      if ((lastElBottom <= scrollCenter && scrollCenter <= elBottom)) {
+        centeredEl = el;
+        break;
+      }
+      lastElBottom = elBottom;
+    }
+    if (centeredEl === null) centeredEl = centerableEls.last();
+    centeredElIndex = centerableEls.index(centeredEl);
+    otherIndex = event.which === 38 ? centeredElIndex - 1 : centeredElIndex + 1;
+    if (!((0 <= otherIndex && otherIndex < centerableEls.length))) return;
+    otherEl = centerableEls.eq(otherIndex);
+    return verticallyCenterTextEl(otherEl);
   };
 
 }).call(this);
