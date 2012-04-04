b5ffdac682c7bf4c8ef170ecb853290dfd224220
Douwe Maan <douwemaan@me.com>
2012-04-04 16:57:38 +0200

    Clean up main.coffee.

--- javascripts/main.coffee
+++ javascripts/main.coffee
@@ -1,100 +1,106 @@
-$(document).ready ->
-  setLastSectionMarginBottom()
-  $(window).on "resize",            setLastSectionMarginBottom
-  $(window).on "orientationchange", setLastSectionMarginBottom
+class PersonalSite
+  constructor: ->
+    @setLastSectionMarginBottom()
+    $(window).on "resize",            @setLastSectionMarginBottom
+    $(window).on "orientationchange", @setLastSectionMarginBottom
   
-  $(window).on "keydown",           handleKeyDown
+    $(window).on "keydown",           @handleKeyDown
   
-  $("header nav a").on "click",     handleNavLinkClick
+    $("header nav a").on "click",     @handleNavLinkClick
   
-  $("h2, ul li", $("section")).on "click", (event) -> focusOnEl event.target
+    $("h2, ul li", $("section")).on "click", (event) => @focusOnEl event.target
   
-setLastSectionMarginBottom = ->  
-  lastSection = $("#traits section").last()
-  lastFocusable = $("h2, ul li", lastSection).last()
-  lastSectionMarginBottom = realWindowHeight() - (focusCenterFromWindowTop() + (lastFocusable.outerHeight() / 2))
+  setLastSectionMarginBottom: =>
+    lastSection = $("#traits section").last()
+    lastFocusable = $("h2, ul li", lastSection).last()
+    lastSectionMarginBottom = @_realWindowHeight() - (@_focusCenterFromWindowTop() + (lastFocusable.outerHeight() / 2))
 
-  lastSection.css "margin-bottom", Math.ceil(lastSectionMarginBottom)
+    lastSection.css "margin-bottom", Math.ceil(lastSectionMarginBottom)
 
-handleNavLinkClick = (event) ->  
-  event.preventDefault()
+  focusOnEl: (focusableEl) ->  
+    focusableEl = $(focusableEl)
+    focusableCenter = focusableEl.offset().top + (focusableEl.outerHeight() / 2)
   
-  link    = $(event.target)
-  href    = link.attr "href"
-  section = $(href)
-  firstFocusable = $("h2, ul li", section).first()
+    $(window).scrollTop Math.ceil(focusableCenter - @_focusCenterFromWindowTop())
+
+  currentlyFocusedEl: (focusableEls = $("h2, ul li", $("section"))) ->
+    # Reverse focusableEls
+    focusableEls = $(focusableEls.get().reverse())
   
-  focusOnEl firstFocusable
+    scrolledFocusCenter = $(window).scrollTop() + @_focusCenterFromWindowTop()
   
-handleKeyDown = (event) ->
-  switch event.which
-    when 13 # Enter
-      event.preventDefault()
+    focusedEl = null
+    lastElTop = 10000 # Arbitrary large number
+    for el in focusableEls
+      el = $(el)
+      offset = el.offset()
+      elTop = offset.top
+      if elTop <= scrolledFocusCenter <= lastElTop
+        focusedEl = el
+        break
+      lastElTop = elTop
+  
+    focusedEl || focusableEls.last()
+
+  # Event handlers
+  handleNavLinkClick: (event) =>  
+    event.preventDefault()
+  
+    link    = $(event.target)
+    href    = link.attr "href"
+    section = $(href)
+    firstFocusable = $("h2, ul li", section).first()
+  
+    @focusOnEl firstFocusable
+  
+  handleKeyDown: (event) =>
+    switch event.which
+      when 13 # Enter
+        event.preventDefault()
       
-      focusedEl = currentlyFocusedEl()
-      link = $("a", focusedEl)
+        focusedEl = @currentlyFocusedEl()
+        link = $("a", focusedEl)
       
-      if link.length
-        url = link.attr "href"
-        if link.attr("target") == "_blank"
-          window.open url
-        else
-          window.location.href = url
+        if link.length
+          url = link.attr "href"
+          if link.attr("target") == "_blank"
+            window.open url
+          else
+            window.location.href = url
       
-    when 38, 40 # Up, Down
-      return if event.metaKey # Don't handle if Windows or Command key was pressed
+      when 38, 40 # Up, Down
+        return if event.metaKey # Don't handle if Windows or Command key was pressed
       
-      event.preventDefault()
+        event.preventDefault()
   
-      if event.altKey # Scroll up/down one section 
-        focusableEls = $("h2, ul:first-child li:first-child", $("section"))
-      else # Scroll up/down one line
-        focusableEls = $("h2, ul li", $("section"))
+        if event.altKey # Scroll up/down one section 
+          focusableEls = $("h2, ul:first-child li:first-child", $("section"))
+        else # Scroll up/down one line
+          focusableEls = $("h2, ul li", $("section"))
 
-      focusedEl = currentlyFocusedEl focusableEls
-      focusedElIndex = focusableEls.index focusedEl
+        focusedEl = @currentlyFocusedEl focusableEls
+        focusedElIndex = focusableEls.index focusedEl
   
-      otherIndex = if event.which == 38 then focusedElIndex - 1 else focusedElIndex + 1
-      return unless 0 <= otherIndex < focusableEls.length
+        otherIndex = if event.which == 38 then focusedElIndex - 1 else focusedElIndex + 1
+        return unless 0 <= otherIndex < focusableEls.length
   
-      otherEl = focusableEls.eq otherIndex
-      focusOnEl otherEl
-
-focusOnEl = (focusableEl) ->  
-  focusableEl = $(focusableEl)
-  focusableCenter = focusableEl.offset().top + (focusableEl.outerHeight() / 2)
+        otherEl = focusableEls.eq otherIndex
+        @focusOnEl otherEl
   
-  $(window).scrollTop Math.ceil(focusableCenter - focusCenterFromWindowTop())
-
-currentlyFocusedEl = (focusableEls = $("h2, ul li", $("section"))) ->
-  # Reverse focusableEls
-  focusableEls = $(focusableEls.get().reverse())
-  
-  scrolledFocusCenter = $(window).scrollTop() + focusCenterFromWindowTop()
-  
-  focusedEl = null
-  lastElTop = 10000 # Arbitrary large number
-  for el in focusableEls
-    el = $(el)
-    offset = el.offset()
-    elTop = offset.top
-    if elTop <= scrolledFocusCenter <= lastElTop
-      focusedEl = el
-      break
-    lastElTop = elTop
-  
-  focusedEl || focusableEls.last()
-  
-realWindowHeight = ->  
-  # $(window).height() != window.innerHeight on iPhone
-  # See http://bugs.jquery.com/ticket/6724
-  if navigator.userAgent.match /(iPhone|iPod)/
-    window.innerHeight
-  else
-    $(window).height()
+  # Private
+  _realWindowHeight: ->  
+    # $(window).height() != window.innerHeight on iPhone
+    # See http://bugs.jquery.com/ticket/6724
+    if navigator.userAgent.match /(iPhone|iPod)/
+      window.innerHeight
+    else
+      $(window).height()
     
-focusCenterFromWindowTop = ->  
-  if navigator.userAgent.match /(iPhone|iPod)/
-    ($("header").outerHeight(true) / 2)
-  else
-    ($(window).height() / 2)
+  _focusCenterFromWindowTop: ->  
+    if navigator.userAgent.match /(iPhone|iPod)/
+      ($("header").outerHeight(true) / 2)
+    else
+      ($(window).height() / 2)    
+
+$(document).ready ->
+  window.personalSite = new PersonalSite
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,111 +1,126 @@
 (function() {
-  var currentlyFocusedEl, focusCenterFromWindowTop, focusOnEl, handleKeyDown, handleNavLinkClick, realWindowHeight, setLastSectionMarginBottom;
+  var PersonalSite,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  PersonalSite = (function() {
+
+    function PersonalSite() {
+      this.handleKeyDown = __bind(this.handleKeyDown, this);
+      this.handleNavLinkClick = __bind(this.handleNavLinkClick, this);
+      this.setLastSectionMarginBottom = __bind(this.setLastSectionMarginBottom, this);
+      var _this = this;
+      this.setLastSectionMarginBottom();
+      $(window).on("resize", this.setLastSectionMarginBottom);
+      $(window).on("orientationchange", this.setLastSectionMarginBottom);
+      $(window).on("keydown", this.handleKeyDown);
+      $("header nav a").on("click", this.handleNavLinkClick);
+      $("h2, ul li", $("section")).on("click", function(event) {
+        return _this.focusOnEl(event.target);
+      });
+    }
+
+    PersonalSite.prototype.setLastSectionMarginBottom = function() {
+      var lastFocusable, lastSection, lastSectionMarginBottom;
+      lastSection = $("#traits section").last();
+      lastFocusable = $("h2, ul li", lastSection).last();
+      lastSectionMarginBottom = this._realWindowHeight() - (this._focusCenterFromWindowTop() + (lastFocusable.outerHeight() / 2));
+      return lastSection.css("margin-bottom", Math.ceil(lastSectionMarginBottom));
+    };
+
+    PersonalSite.prototype.focusOnEl = function(focusableEl) {
+      var focusableCenter;
+      focusableEl = $(focusableEl);
+      focusableCenter = focusableEl.offset().top + (focusableEl.outerHeight() / 2);
+      return $(window).scrollTop(Math.ceil(focusableCenter - this._focusCenterFromWindowTop()));
+    };
+
+    PersonalSite.prototype.currentlyFocusedEl = function(focusableEls) {
+      var el, elTop, focusedEl, lastElTop, offset, scrolledFocusCenter, _i, _len;
+      if (focusableEls == null) focusableEls = $("h2, ul li", $("section"));
+      focusableEls = $(focusableEls.get().reverse());
+      scrolledFocusCenter = $(window).scrollTop() + this._focusCenterFromWindowTop();
+      focusedEl = null;
+      lastElTop = 10000;
+      for (_i = 0, _len = focusableEls.length; _i < _len; _i++) {
+        el = focusableEls[_i];
+        el = $(el);
+        offset = el.offset();
+        elTop = offset.top;
+        if ((elTop <= scrolledFocusCenter && scrolledFocusCenter <= lastElTop)) {
+          focusedEl = el;
+          break;
+        }
+        lastElTop = elTop;
+      }
+      return focusedEl || focusableEls.last();
+    };
+
+    PersonalSite.prototype.handleNavLinkClick = function(event) {
+      var firstFocusable, href, link, section;
+      event.preventDefault();
+      link = $(event.target);
+      href = link.attr("href");
+      section = $(href);
+      firstFocusable = $("h2, ul li", section).first();
+      return this.focusOnEl(firstFocusable);
+    };
+
+    PersonalSite.prototype.handleKeyDown = function(event) {
+      var focusableEls, focusedEl, focusedElIndex, link, otherEl, otherIndex, url;
+      switch (event.which) {
+        case 13:
+          event.preventDefault();
+          focusedEl = this.currentlyFocusedEl();
+          link = $("a", focusedEl);
+          if (link.length) {
+            url = link.attr("href");
+            if (link.attr("target") === "_blank") {
+              return window.open(url);
+            } else {
+              return window.location.href = url;
+            }
+          }
+          break;
+        case 38:
+        case 40:
+          if (event.metaKey) return;
+          event.preventDefault();
+          if (event.altKey) {
+            focusableEls = $("h2, ul:first-child li:first-child", $("section"));
+          } else {
+            focusableEls = $("h2, ul li", $("section"));
+          }
+          focusedEl = this.currentlyFocusedEl(focusableEls);
+          focusedElIndex = focusableEls.index(focusedEl);
+          otherIndex = event.which === 38 ? focusedElIndex - 1 : focusedElIndex + 1;
+          if (!((0 <= otherIndex && otherIndex < focusableEls.length))) return;
+          otherEl = focusableEls.eq(otherIndex);
+          return this.focusOnEl(otherEl);
+      }
+    };
+
+    PersonalSite.prototype._realWindowHeight = function() {
+      if (navigator.userAgent.match(/(iPhone|iPod)/)) {
+        return window.innerHeight;
+      } else {
+        return $(window).height();
+      }
+    };
+
+    PersonalSite.prototype._focusCenterFromWindowTop = function() {
+      if (navigator.userAgent.match(/(iPhone|iPod)/)) {
+        return $("header").outerHeight(true) / 2;
+      } else {
+        return $(window).height() / 2;
+      }
+    };
+
+    return PersonalSite;
+
+  })();
 
   $(document).ready(function() {
-    setLastSectionMarginBottom();
-    $(window).on("resize", setLastSectionMarginBottom);
-    $(window).on("orientationchange", setLastSectionMarginBottom);
-    $(window).on("keydown", handleKeyDown);
-    $("header nav a").on("click", handleNavLinkClick);
-    return $("h2, ul li", $("section")).on("click", function(event) {
-      return focusOnEl(event.target);
-    });
+    return window.personalSite = new PersonalSite;
   });
-
-  setLastSectionMarginBottom = function() {
-    var lastFocusable, lastSection, lastSectionMarginBottom;
-    lastSection = $("#traits section").last();
-    lastFocusable = $("h2, ul li", lastSection).last();
-    lastSectionMarginBottom = realWindowHeight() - (focusCenterFromWindowTop() + (lastFocusable.outerHeight() / 2));
-    return lastSection.css("margin-bottom", Math.ceil(lastSectionMarginBottom));
-  };
-
-  handleNavLinkClick = function(event) {
-    var firstFocusable, href, link, section;
-    event.preventDefault();
-    link = $(event.target);
-    href = link.attr("href");
-    section = $(href);
-    firstFocusable = $("h2, ul li", section).first();
-    return focusOnEl(firstFocusable);
-  };
-
-  handleKeyDown = function(event) {
-    var focusableEls, focusedEl, focusedElIndex, link, otherEl, otherIndex, url;
-    switch (event.which) {
-      case 13:
-        event.preventDefault();
-        focusedEl = currentlyFocusedEl();
-        link = $("a", focusedEl);
-        if (link.length) {
-          url = link.attr("href");
-          if (link.attr("target") === "_blank") {
-            return window.open(url);
-          } else {
-            return window.location.href = url;
-          }
-        }
-        break;
-      case 38:
-      case 40:
-        if (event.metaKey) return;
-        event.preventDefault();
-        if (event.altKey) {
-          focusableEls = $("h2, ul:first-child li:first-child", $("section"));
-        } else {
-          focusableEls = $("h2, ul li", $("section"));
-        }
-        focusedEl = currentlyFocusedEl(focusableEls);
-        focusedElIndex = focusableEls.index(focusedEl);
-        otherIndex = event.which === 38 ? focusedElIndex - 1 : focusedElIndex + 1;
-        if (!((0 <= otherIndex && otherIndex < focusableEls.length))) return;
-        otherEl = focusableEls.eq(otherIndex);
-        return focusOnEl(otherEl);
-    }
-  };
-
-  focusOnEl = function(focusableEl) {
-    var focusableCenter;
-    focusableEl = $(focusableEl);
-    focusableCenter = focusableEl.offset().top + (focusableEl.outerHeight() / 2);
-    return $(window).scrollTop(Math.ceil(focusableCenter - focusCenterFromWindowTop()));
-  };
-
-  currentlyFocusedEl = function(focusableEls) {
-    var el, elTop, focusedEl, lastElTop, offset, scrolledFocusCenter, _i, _len;
-    if (focusableEls == null) focusableEls = $("h2, ul li", $("section"));
-    focusableEls = $(focusableEls.get().reverse());
-    scrolledFocusCenter = $(window).scrollTop() + focusCenterFromWindowTop();
-    focusedEl = null;
-    lastElTop = 10000;
-    for (_i = 0, _len = focusableEls.length; _i < _len; _i++) {
-      el = focusableEls[_i];
-      el = $(el);
-      offset = el.offset();
-      elTop = offset.top;
-      if ((elTop <= scrolledFocusCenter && scrolledFocusCenter <= lastElTop)) {
-        focusedEl = el;
-        break;
-      }
-      lastElTop = elTop;
-    }
-    return focusedEl || focusableEls.last();
-  };
-
-  realWindowHeight = function() {
-    if (navigator.userAgent.match(/(iPhone|iPod)/)) {
-      return window.innerHeight;
-    } else {
-      return $(window).height();
-    }
-  };
-
-  focusCenterFromWindowTop = function() {
-    if (navigator.userAgent.match(/(iPhone|iPod)/)) {
-      return $("header").outerHeight(true) / 2;
-    } else {
-      return $(window).height() / 2;
-    }
-  };
 
 }).call(this);
